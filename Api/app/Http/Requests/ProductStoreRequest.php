d71546e160db88cab1b1307bddca4db6f3ab26e5
Kawê <kawe.apr@gmail.com>
2023-09-06 21:38:38 -0300

    new models / store function / API routes

---
+++ Api/app/Http/Requests/ProductStoreRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        if(request()->isMethod('post')){
+
+            return [
+                'name' => 'required|string|max:258',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'description' => 'required|string',
+            ];
+        }else{
+
+            return [
+                'name' => 'required|string|max:258',
+                'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'description' => 'required|string',
+            ];
+        }
+    }
+    public function messages()
+    {
+        if(request()->isMethod('post')){
+
+            return [
+                'name.required' => 'É obrigatório adicionar um nome!',
+                'image' => 'É obrigatório adicionar uma imagem!',
+                'description' => 'É obrigatório existir uma descrição!',
+            ];
+        }else{
+
+            return [
+                'name.required' => 'É obrigatório adicionar um nome!',
+                'description' => 'É obrigatório existir uma descrição!',
+            ];
+        }
+    }
+}
